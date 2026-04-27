2e6481509fa21f1fda6d750d9c4df2a4643463fc
alejandromelero26 <amelevel@myuax.com>
2026-04-27 11:06:56 +0200

    TipoEntrenador

--- Prog_LigaFutbol-master/src/LigaFutbol/Modelo/Enumeraciones/TipoEntrenador.java
+++ Prog_LigaFutbol-master/src/LigaFutbol/Modelo/Enumeraciones/TipoEntrenador.java
@@ -1,4 +1,23 @@
 package LigaFutbol.Modelo.Enumeraciones;
 
-public class TipoEntrenador {
+public enum TipoEntrenador {
+
+    primerEntrenador(1, "Mister"),
+    segundoEntrenador(2, "Segundo mister");
+
+    private int valor;
+    private String descripcion;
+
+    TipoEntrenador(int valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
